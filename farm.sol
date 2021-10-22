fa9d524e92c0e323586de0dfd66694728478f1b3
DX <84550424+qinfengdx@users.noreply.github.com>
2021-10-22 14:40:44 +0800

    Update farm.sol

--- farm.sol
+++ farm.sol
@@ -7,10 +7,13 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "./SafeMath.sol";
 import "./MyERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract Farm is Context,SafeControl,Ownable {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
     address private minter;//管理员
-    address private myERC20Addr=0xeca182999fe00D41e229e4061CA40954B8EaE5ED;//MBT address
+    address private myERC20Addr=0x84281CBF804dd2A49e3227d6E65606490CF9C98A;//MBT address
     MyERC20 private myerc20=MyERC20(myERC20Addr);
     uint256 private aproveunlocked=1;//领取锁，防止重放攻击
     bool private adminOperatelocked=false;//管理员进行 算力写入和donate时加锁，防止此时领取
@@ -23,7 +26,7 @@ contract Farm is Context,SafeControl,Ownable {
     uint256 public lastRewardBlock;//上次结算时区块
     uint256 public rewardTokenPerBlock;//每个区块的奖励
 
-
+   
     constructor(uint256 _rewardTokenPerBlock) {
         minter=_msgSender();//合约创建者为管理员
         rewardTokenPerBlock = _rewardTokenPerBlock;//定义每个区块的token奖励
@@ -31,31 +34,38 @@ contract Farm is Context,SafeControl,Ownable {
         
     }
    /*...
-    函数名：update  
-    更新用户算力，更新总算力，并结算捐赠 ，置零累计领取，更新上次结算时区块。
+    函数名：setupdate
+    更新用户算力
     */
-    function setupdate(address[] memory _tos, uint256[] memory _computPower,uint _rewardTokenPerBlock,uint _pice)  public adminOperatelock returns(bool) {
+    function setupdate(address[] memory _tos, uint256[] memory _computPower,uint _pice)  public adminOperatelock returns(bool) {
+        require(Address.isContract(_msgSender())==false,"CA");
+        require(_msgSender()==minter,"not admin");
+        //更新算力 写入下一个pice周期的算力 先不更新pice,批量写入完毕后再调用setupdateEnd 
+        uint count=_tos.length;
+        for(uint i=0;i<count;i++){
+            totalAllocPoint[_pice]=totalAllocPoint[_pice].add(_computPower[i]);//更新 累加总算力
+            allocPoint[_pice][_tos[i]]=_computPower[i];//更新算力
+        }
+        return true;
+    }
+     /*...
+    函数名：setupdateEnd 
+    更新 并结算捐赠 ，置零累计领取，更新上次结算时区块。
+    */
+    function setupdateEnd(uint _rewardTokenPerBlock,uint _pice)  public adminOperatelock returns(bool) {
         require(Address.isContract(_msgSender())==false,"CA");
         require(_msgSender()==minter,"not admin");
         //更新每个区块的奖励
         rewardTokenPerBlock=_rewardTokenPerBlock;
         pice=_pice;//更新周期 
-        //更新算力
-        uint count=_tos.length;
-        for(uint i=0;i<count;i++){
-            totalAllocPoint[pice]=totalAllocPoint[pice].add(_computPower[i]);//更新 累加总算力
-            allocPoint[pice][_tos[i]]=_computPower[i];//更新算力
-        }
         uint256 blockReward = getRewardTokenBlockReward();//计算从上次更新算力后的 累计区块 奖励
         uint256 lessToken=blockReward-totalReaped;
         totalReaped=0;
         lastRewardBlock =  block.number;//新的区块起点
-        myerc20.mint(address(this),lessToken);//mint剩余区块的累计奖励 到合约
-        //账户合约全部余额捐赠
-        uint256 donateToken= myerc20.balanceOf(address(this));
-        myerc20.transfer(address(myERC20Addr),donateToken);//MBT转移至MBT合约账户  捐赠
-            
-        
+        require(myerc20.mint(address(this),lessToken),"mint err");//mint剩余区块的累计奖励 到合约
+        // //账户合约全部余额捐赠
+        // uint256 donateToken= myerc20.balanceOf(address(this));
+        myerc20.donate(lessToken);//MBT转移至MBT合约账户  捐赠
         return true;
     }
     /*...
@@ -68,17 +78,7 @@ contract Farm is Context,SafeControl,Ownable {
         minter=addr;
         return true;
     }
-    /*...
-    函数名：mint  
-    给本合约也就是Farm合约发送指定_amount数量的MBT
-    */
-    function mint(uint256 _amount) public lock adminOperatelock returns (bool) {
-        require(Address.isContract(_msgSender())==false&&_amount>0,"CA");
-        require(_msgSender()==minter,"not admin");
-        myerc20.mint(address(this),_amount);
-        return true;
-    }
-    
+   
     /*...
     函数名：computationalPowerOf 
     单个查询 addr地址本周  算力
@@ -129,7 +129,7 @@ contract Farm is Context,SafeControl,Ownable {
         allocPoint[pice][_msgSender()]=0;//用户算力归零  下次更新算力前不能再收获了
         totalReaped= totalReaped.add(tokenReward);//记录已领取的
         require(myerc20.mint(address(this),tokenReward), "mint error");//mint 奖励 tokenReward 到合约
-        myerc20.transfer(_msgSender(),tokenReward);//MBT转移 user能够获得的 tokenReward 至用户账户
+       // myerc20.safeTransfer(_msgSender(),tokenReward);//MBT转移 user能够获得的 tokenReward 至用户账户
         return true;
     }
     
